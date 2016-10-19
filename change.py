af99432b9614c4041bd41950f612cde0b7b99587
donoghuc <cas.donoghue@gmail.com>
2016-10-18 19:25:52 -0700

    first commit

---
+++ change.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+#Cas Donoghue
+#Patrick Kwong
+#Nicholas Vrontakis
+#Project 2 CS325 
+
+# sys for the -i file
+import sys
+from changedpFunction import changedp
+
+#store file input
+fileInput = sys.argv[1]
+#simple error indicator, should probably just write a main function 
+error = 0
+#tiny bit of input validation
+try:
+	fo = open(fileInput,"r")
+except IOError:
+	error = 1
+	print('cannot open',fileInput)
+
+# if user managed to specify an existing file, then do the work. 
+if (error != 1):
+	#store very input line as array of arrays
+	arrayOfArrays = []
+
+	for line in fo:
+		#ignore blank lines
+		if not line.strip():
+			garbage = 1 #why do you have to have something here? Does anybody have suggestions for parsing? 
+		else:
+			line = line.replace("[","")
+			line = line.replace("]","")
+			line = line.replace(" ","")
+			line = line.replace('\n',"")
+			#parsing on comma
+			array = [int(x) for x in line.split(",")]
+			#make array of arrays for use in all 4 algorithms. 
+			arrayOfArrays.append(array)
+
+	for i in range(0,len(arrayOfArrays),2):
+		changedp(arrayOfArrays[i],arrayOfArrays[i+1][0])
+
+
+
+
+   
+# #declare an array of denominations prob1
+# coinValueList = [1,3,4]
+# #call function
+# dpMakeChange(coinValueList,11)
+# #declare prob 2 denoms
+# coinValueList1 = [1,10,25,50]
+# #call function
+# dpMakeChange(coinValueList1,40)
+
---
+++ changedpFunction.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#Cas Donoghue
+#Patrick Kwong
+#Nicholas Vrontakis
+#Project 2 algorithm 4 DP
+
+def changedp(coinValueList,change):
+   coinsUsed = [0]*(change+1) #keep track of "new" coin that was just "added" to solution
+   minCoins = [0]*(change+1)  #keep track of min num coins for each value
+   for i in range(0,change+1): #loop through n. n is change you want to make
+      coinCount = i #start with 0 end at n
+      newCoin = 1 # assume 1 new coin added
+      for a in range(0,len(coinValueList)): # loop through possible coin denominations. 
+        if(coinValueList[a] <= i):  # only consider denomiantions that can make the current change 
+          if(minCoins[i-coinValueList[a]] + 1 < coinCount): # important! if you can make the change in less than
+#                                                            the previous change value plus the lowest denomination
+#                                                             then you just add the relevent change and update arrays
+            coinCount = minCoins[i - coinValueList[a]] + 1 # this tracks lowest num of coins to get current value of i
+            newCoin = coinValueList[a] # this value tracks the denomination that was not the lowest denomination
+      minCoins[i] = coinCount #just storing in array to be accessible in main, values explained above
+      coinsUsed[i] = newCoin
+      
+   solutionList = []
+   denomIndex = change
+   while (denomIndex > 0):
+    solutionList.append(coinsUsed[denomIndex])
+    denomIndex -= coinsUsed[denomIndex]
+   print "Num coins: " + str(minCoins[change]) # min number of total coins will be last element of min array (n)
+   print "Coins used: " + str(solutionList)  #print solutions
