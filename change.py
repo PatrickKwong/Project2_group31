d61f9b2c32184921b4a2ba76b742f8d7453cd4d9
niktak11 <niktak11@yahoo.com>
2016-10-22 13:35:16 -0700

    greedy algo

--- change.py
+++ change.py
@@ -2,7 +2,7 @@
 #Cas Donoghue
 #Patrick Kwong
 #Nicholas Vrontakis
-#Project 2 CS325 
+#Project 2 CS325
 
 # Call the program with one arguement (formatted .txt file) example: >>python change.py Amount.txt
 # will print results to [userinput]change.txt in same directory
@@ -13,7 +13,7 @@ from changedpFunction import changedp
 
 #store file input
 fileInput = sys.argv[1]
-#simple error indicator, should probably just write a main function 
+#simple error indicator, should probably just write a main function
 error = 0
 #tiny bit of input validation
 try:
@@ -22,51 +22,36 @@ except IOError:
 	error = 1
 	print('cannot open',fileInput)
 
-# if user managed to specify an existing file, then do the work. 
+# if user managed to specify an existing file, then do the work.
 if (error != 1):
 	#store very input line as array of arrays
 	arrayOfArrays = []
 
 	for line in fi:
 		#ignore blank lines
-		if not line.strip():
-			garbage = 1 #why do you have to have something here? Does anybody have suggestions for parsing? 
-		else:
+		if line.strip():
 			line = line.replace("[","")
 			line = line.replace("]","")
 			line = line.replace(" ","")
 			line = line.replace('\n',"")
 			#parsing on comma
 			array = [int(x) for x in line.split(",")]
-			#make array of arrays for use in all 4 algorithms. 
+			#make array of arrays for use in all 4 algorithms.
 			arrayOfArrays.append(array)
 	fi.close()
 
 	fileOutput = fileInput[:len(fileInput)-4] + 'change.txt'
 
-
-	fo = open(fileOutput,"w+")
-
-	for i in range(0,len(arrayOfArrays),2):
-		change,numCoins = changedp(arrayOfArrays[i],arrayOfArrays[i+1][0])
-		fo.write(change + '\n') 
-		fo.write(numCoins + '\n')
-
-	
-	fo.close()
+        with open(fileOutput,"w+") as fo:
+                for i in range(0,len(arrayOfArrays),2):
+		        change,numCoins = changedp(arrayOfArrays[i],arrayOfArrays[i+1][0])
+		        fo.write(change + '\n')
+		        fo.write(numCoins + '\n')
 
 	print 'results in ' + fileOutput
-
-
-
-	
-
 	print 'branch test'
 
 
-
-
-   
 # #declare an array of denominations prob1
 # coinValueList = [1,3,4]
 # #call function
