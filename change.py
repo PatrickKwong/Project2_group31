fe9d1a44fa968e1149f08c959cc8d7f5cad81bd7
niktak11 <niktak11@yahoo.com>
2016-10-23 16:50:13 -0700

    added runtime data output - can be turned off by setting output_runtime_data to False

--- change.py
+++ change.py
@@ -10,9 +10,14 @@
 
 # sys for the -i file
 import sys
+import csv
+import time
 from changeslowFunction import changeslow
 from changedpFunction import changedp
 from change_greedy import get_change
+
+output_runtime_data = True
+runtime_data = []
 
 #store file input
 fileInput = sys.argv[1]
@@ -44,19 +49,31 @@ if (error != 1):
 	fi.close()
 
 	fileOutput = fileInput[:len(fileInput)-4] + 'change.txt'
-
 	with open(fileOutput,"w+") as fo:
 		for i in range(0,len(arrayOfArrays),2):
+                        testcase_runtime_data = []
+                        cur_time = time.clock()
 			change, numCoins = get_change(arrayOfArrays[i],arrayOfArrays[i+1][0])
+                        testcase_runtime_data.append(round(time.clock() - cur_time, 7))
 			fo.write("Algorithm changeslow:\n{0}\n{1}\n".format(change, numCoins))
+                        cur_time = time.clock()
 			change, numCoins = get_change(arrayOfArrays[i],arrayOfArrays[i+1][0])
+                        testcase_runtime_data.append(round(time.clock() - cur_time, 7))
 			fo.write("Algorithm changegreedy:\n{0}\n{1}\n".format(change, numCoins))
+                        cur_time = time.clock()
 			change, numCoins = changedp(arrayOfArrays[i],arrayOfArrays[i+1][0])
+                        testcase_runtime_data.append(round(time.clock() - cur_time, 7))
 			fo.write("Algorithm changedp:\n{0}\n{1}\n".format(change, numCoins))
+                        runtime_data.append(testcase_runtime_data)
 
 	print 'results in ' + fileOutput
 	print 'branch test'
 
+        if (output_runtime_data):
+                with open("runtime_data.csv", "w+") as csv_file:
+                        csv_writer = csv.writer(csv_file, delimiter=',')
+                        for algo_data in runtime_data:
+                                csv_writer.writerow(algo_data)
 
 # #declare an array of denominations prob1
 # coinValueList = [1,3,4]
